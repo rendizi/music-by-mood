f3fa5b11486ef1c1e907c0b21f890471da3b5f1a
rendizi <baglanov.a0930@gmail.com>
2024-09-20 20:57:19 +0500

    prompt changed

--- src/app/api/process-frame/route.ts
+++ src/app/api/process-frame/route.ts
@@ -17,7 +17,7 @@ export async function POST(req: NextRequest) {
           {
             role: "user",
             content: [
-              { type: "text", text: "Based on the person's emotions, people around them, gender, and background, recommend a song. The suggestion should be trendy, nostalgic, or viral on TikTok, and should only include real songs that exist on Spotify. Provide the song title in the original language if it's a non-English song (e.g., Kazakh). The response should only contain the song title, and you must verify the song exists, dont return any other text! Be creative with music" },
+              { type: "text", text: "Based on the person's emotions, people around them, gender, and background, recommend a song. The suggestion should be trendy, nostalgic, soundtrack from popular film, or viral on TikTok, and should only include real songs that exist on Spotify. Provide the song title in the original language if it's a non-English song (e.g., Kazakh). The response should only contain the song title, and you must verify the song exists, dont return any other text(including author)! Be creative with music" },
               {
                 type: "image_url",
                 image_url: {
